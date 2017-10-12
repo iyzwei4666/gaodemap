b3818c66a2dfbb45771b87cbe1f3f04c5418fd0a
weiyongzhi <123456>
2017-10-12 14:27:06 +0800

    位置

---
+++ mylibrary/src/main/java/com/wyz/mylibrary/OfflineMapCity.java
@@ -0,0 +1,97 @@
+package com.wyz.mylibrary;
+
+/**
+ * Created by WYZ on 2017/10/12.
+ */
+
+public  class OfflineMapCity {
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    private  String city;
+    private  String url;
+    private long size;
+    private int state;
+    private int completeCode;
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public long getSize() {
+        return size;
+    }
+
+    public void setSize(long size) {
+        this.size = size;
+    }
+
+    public int getState() {
+        return state;
+    }
+
+    public void setState(int state) {
+        this.state = state;
+    }
+
+    public int getcompleteCode() {
+        return completeCode;
+    }
+
+    public void setCompleteCode(int completeCode) {
+        this.completeCode = completeCode;
+    }
+
+    //    public String getUrl() {
+//        return this.a;
+//    }
+//
+//    public void setUrl(String var1) {
+//        this.a = var1;
+//    }
+//
+//    public long getSize() {
+//        return this.b;
+//    }
+//
+//    public void setSize(long var1) {
+//        this.b = var1;
+//    }
+//
+//    public int getState() {
+//        return this.c;
+//    }
+//
+//    public void setState(int var1) {
+//        this.c = var1;
+//    }
+//
+//    public String getVersion() {
+//        return this.d;
+//    }
+//
+//    public void setVersion(String var1) {
+//        this.d = var1;
+//    }
+//
+//    public int getcompleteCode() {
+//        return this.e;
+//    }
+//
+//    public void setCompleteCode(int var1) {
+//        this.e = var1;
+//    }
+
+    public int describeContents() {
+        return 0;
+    }
+}
---
+++ mylibrary/src/main/java/com/wyz/mylibrary/OfflineMapManager.java
@@ -0,0 +1,67 @@
+package com.wyz.mylibrary;
+
+import android.content.Context;
+
+import java.util.List;
+
+/**
+ * Created by WYZ on 2017/10/12.
+ */
+
+public class OfflineMapManager {
+
+
+    public OfflineMapManager(Context activity, OfflineMapDownloadListener Listener) {
+
+    }
+
+    public void downloadByCityName(String city) {
+    }
+
+    public void downloadByProvinceName(String city) {
+    }
+
+    public void pause() {
+    }
+
+    public void restart() {
+    }
+
+    public void remove(String name) {
+    }
+
+    public void updateOfflineCityByName(String name) {
+    }
+
+    public List<OfflineMapCity> getDownloadOfflineMapCityList() {
+        return null;
+    }
+    public List<OfflineMapCity> getDownloadingCityList() {
+        return null;
+    }
+
+    public void destroy() {
+    }
+
+    public void stop() {
+    }
+
+    public void setOnOfflineLoadedListener(OfflineMapDownloadListener listener) {
+    }
+
+    public List<OfflineMapProvince> getOfflineMapProvinceList() {
+        return null;
+    }
+
+    public interface OfflineLoadedListener {
+        void onVerifyComplete();
+    }
+
+    public interface OfflineMapDownloadListener {
+        void onDownload(int status, int completeCode, String downName) ;
+
+        void onCheckUpdate(boolean hasNew, String name);
+
+        void  onRemove(boolean success, String name, String describe);
+    }
+}
